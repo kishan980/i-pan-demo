58a978badd9a9169090b7fe4a3d59189b2885170
unknown <kishany980>
2023-09-09 12:15:55 +0530

    please postmanurl file sir url diya hai and post main mai koun sa method use kar na sir diya hai maine

---
+++ postman-url.js
@@ -0,0 +1,17 @@
+post => localhost:5000/manager/departments
+patch=> localhost:5000/manager/64f8740c14b94b83f84df5a1
+get =>  localhost:5000/manager/departments
+get=>   localhost:5000/manager/departments?page=5
+get=>   localhost:5000/manager/64f5bcb1e7c5383897b40a38
+delete=>   localhost:5000/manager/64f8740e14b94b83f84df5a3
+get=>    localhost:5000/manager/employees-in-it-with-location-a?department=IT&location=A
+get=>  localhost:5000/manager/employees-in-sales?department=Sales&firstName=-1&lastName=-1
+
+
+
+
+localhost:5000/employee/login
+localhost:5000/employee/signup
+
+
+https://api.postman.com/collections/8108030-b60cf2d0-f4a8-4861-8198-80c7217e816e?access_key=PMAT-01H9N8VRQJT40XRAC0X30S29KD
